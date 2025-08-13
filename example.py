f0174fa66fa1513d227a2f2cd744d6820f5924aa
0N3R0 <dawid175571@gmail.com>
2025-08-13 17:56:12 +0200

    Made some changes

--- example.py
+++ example.py
@@ -7,15 +7,15 @@ async def main() -> None:
 
     start: float = time.perf_counter()
 
-    cf_verify: CFVerify = CFVerify(_browser_tab=browser_tab, _debug=True)
-    result: bool = await cf_verify.verify(_max_retries=10, _interval_between_retries=1, _reload_page_after_n_retries=0)
+    cf_verify: CFVerify = CFVerify(_browser_tab = browser_tab, _debug = True)
+    success: bool = await cf_verify.verify(_max_retries = 15, _interval_between_retries = 1, _reload_page_after_n_retries = 0)
 
     duration: float = (time.perf_counter() - start)
 
-    if result:
-        print(f"Cloudflare was successfully verified in {duration:.2f} seconds.")
+    if not success:
+        print(f"Failed to verify Cloudflare. Elapsed time: {duration:.2f} seconds.")
         return
-    
-    print(f"Failed to verify Cloudflare. Elapsed time: {duration:.2f} seconds.")
 
-nodriver.loop().run_until_complete(main())
+    print(f"Cloudflare was successfully verified in {duration:.2f} seconds.")
+
+nodriver.loop().run_until_complete(future = main())
